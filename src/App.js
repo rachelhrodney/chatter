cb1d39b65a4257689030783e5608ad644927849b
Rachel Rodney <rachelrodney@D-10-19-107-75.dhcp4.washington.edu>
2019-07-01 12:59:33 -0700

    react

--- src/App.js
+++ src/App.js
@@ -3,17 +3,29 @@ import './App.css';
 import logo from './kittyLogo.png';
 import TextInput from './TextInput'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="header">
-      <img src={logo} className="logo" alt="logo" />
-        Chatter
+class App extends React.Component {
+  state = {
+    messages: []
+  }
+
+  sendMessage = (m) => {
+    var messages = [...this.state.messages, m]
+    this.setState({ messages })
+  }
+
+  render() {
+    console.log(this.state.messages)
+    return (
+      <div className="App">
+        <header className="header">
+          <img src={logo} className="logo" alt="logo" />
+          Chatter
         </header>
-        <TextInput />
-        
-    </div>
-  );
+        <TextInput sendMessage={this.sendMessage} />
+
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/TextInput.js
+++ src/TextInput.js
@@ -1,14 +1,37 @@
 import React from 'react'
+import { FiSend } from "react-icons/fi";
 
 class TextInput extends React.Component {
 
+    state={
+        text:"",
+    }
+
+    send = () => {
+        this.props.sendMessage(this.state.text)
+        this.setState({text:""})
+    }
+
+    keyPress = (e) => {
+        //console.log(e)
+        if(e.key==='Enter') {
+            this.send()
+        }
+    }
+
     render(){
         return(<div className='text-input'>
-            <input />
-            <div class="bubble">
+            <input value={this.state.text}
+                placeholder="Write your message here..."
+                onChange={e=> this.setState({text: e.target.value})}
+                onKeyPress={this.keyPress}
+            />
+            <div className="bubble">
             Hello !!
             </div>
-            <button>--></button>
+            <button disabled={!this.state.text} onClick={this.send}>
+                <FiSend style={{height:15,width:15}}/>
+            </button>
         </div>)
     }
 
